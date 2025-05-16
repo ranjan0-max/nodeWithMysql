0019d8781ce0d32196ea81cfde0b7c7c77679e0f
Ranjan Chauhan <ranjan13101997@gmail.com>
2025-05-16 20:30:18 +0530

    Delete Helpers directory

--- Helpers/removeNullKeys.helper.js
+++
@@ -1,7 +0,0 @@
-const removeNullKeys = (obj) => {
-    return Object.fromEntries(Object.entries(obj).filter(([_, value]) => value !== null && value !== undefined));
-};
-
-module.exports = {
-    removeNullKeys
-};
--- Helpers/uniqueIdGenerator.helper.js
+++
@@ -1,9 +0,0 @@
-const generateUniqueId = (prefix = 'UID') => {
-    const timestamp = Date.now();
-    const random = Math.floor(1000 + Math.random() * 9000);
-    return `${prefix}_${timestamp}_${random}`;
-};
-
-module.exports = {
-    generateUniqueId
-};
