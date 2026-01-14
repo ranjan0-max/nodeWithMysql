49a4e5fa9c8cee5f03087c94ede3f8f03add460b
Ranjan Chauhan <ranjan13101997@gmail.com>
2026-01-14 14:20:22 +0530

    Refactor CRUD helper functions for better structure

--- Helpers/crud.helper.js
+++ Helpers/crud.helper.js
@@ -1,94 +1,172 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// Remove specified keys from data object(s)
-function removeKeys(keys, data) {
-  const dataArray = Array.isArray(data) ? data : [data];
-  return dataArray.map((item) => {
-    keys.forEach((key) => {
-      if (key === '_id') {
-        item.id = item[key];
+function isUnique(model, data) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const exists = await model.findFirst({ where: data });
+
+      if (exists) {
+        reject({ message: "Data already exists" });
+      } else {
+        resolve(true);
       }
-      delete item[key];
-    });
-    return item;
+    } catch (err) {
+      reject(err);
+    }
   });
 }
 
-// Check uniqueness
-async function isUnique(model, data) {
-  const count = await model.count({ where: data });
-  if (count > 0) {
-    throw new Error('Data exists!');
-  }
-  return true;
-}
-
-// Get count
-async function getCount(model, query = {}) {
-  return await model.count({ where: query });
-}
-
-// Create record
-async function create(model, data) {
-  const createdData = await model.create({ data });
-  return removeKeys(['id'], createdData);
-}
-
-// Read with pagination
-async function readWithPagination(model, query, pagination = {}, exclude = [], orderBy = []) {
-  const data = await model.findMany({
-    where: query,
-    select: exclude.length ? Object.fromEntries(exclude.map((key) => [key, false])) : undefined,
-    skip: pagination.skip || 0,
-    take: pagination.limit || 10,
-    orderBy,
+function getCount(model, query = {}) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const count = await model.count({ where: query });
+      resolve(count);
+    } catch (err) {
+      reject(err);
+    }
   });
-  return data;
 }
 
-// Read single record
-async function read(model, query, exclude = [], orderBy = []) {
-  const data = await model.findFirst({
-    where: query,
-    select: exclude.length ? Object.fromEntries(exclude.map((key) => [key, false])) : undefined,
-    orderBy,
+function create(model, data) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await model.create({ data });
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
   });
-  return data;
 }
 
-// Update record
-async function update(model, query, data) {
-  return await model.updateMany({ where: query, data });
+
+function createMany(model, data) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await model.createMany({ data });
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
 
-// Delete record
-async function remove(model, query) {
-  return await model.deleteMany({ where: query });
+
+function read(model, query, select = [], orderBy = []) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await model.findFirst({
+        where: query,
+        select: select.length
+          ? Object.fromEntries(select.map((key) => [key, true]))
+          : undefined,
+        orderBy
+      });
+
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
 
-// Find details with includes
-async function findDetails(model, query, includes = []) {
-  return await model.findMany({ where: query, include: includes.length ? Object.fromEntries(includes.map((key) => [key, true])) : undefined });
+function updateMany(model, query, data) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await model.updateMany({
+        where: query,
+        data
+      });
+
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
 
-// Find details with selected fields
-async function findDetailsWithSelectedField(model, query, projection = [], includes = []) {
-  return await model.findMany({
-    where: query.conditions || {},
-    select: projection.length ? Object.fromEntries(projection.map((key) => [key, true])) : undefined,
-    include: includes.length ? Object.fromEntries(includes.map((key) => [key, true])) : undefined,
+function updateOne(model, query, data) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await model.update({
+        where: query,
+        data
+      });
+
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+function remove(model, query) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await model.deleteMany({
+        where: query
+      });
+
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+function findManyDetails(
+  model,
+  query,
+  logical = 'AND',
+  projection = [],
+  include = [],
+  orderBy = [],
+  pagination = {}
+) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let whereClause = {};
+
+      if (Array.isArray(query)) {
+        if (logical === 'OR') {
+          whereClause = { OR: query };
+        } else if (logical === 'AND') {
+          whereClause = { AND: query };
+        } else {
+          throw new Error("Invalid logical operator. Use 'AND' or 'OR'.");
+        }
+      } else if (query && typeof query === 'object') {
+        whereClause = query;
+      }
+
+      const result = await model.findMany({
+        where: whereClause,
+        select: projection.length
+          ? Object.fromEntries(projection.map((key) => [key, true]))
+          : undefined,
+        include: include.length
+          ? Object.fromEntries(include.map((key) => [key, true]))
+          : undefined,
+        skip: pagination.skip ? pagination.skip : undefined,
+        take: pagination.limit ? pagination.limit : undefined,
+        orderBy: orderBy.length ? orderBy : undefined
+      });
+
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
   });
 }
 
 module.exports = {
   isUnique,
   getCount,
-  readWithPagination,
   read,
   create,
-  update,
+  createMany,
+  updateMany,
+  updateOne,
   remove,
-  findDetails,
-  findDetailsWithSelectedField,
+  findManyDetails
 };
